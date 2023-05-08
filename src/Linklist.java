33c08307d5d55ba045be9b5ea84562915b5140eb
lazy-codebear <tonys65536@gmail.com>
2023-05-08 20:46:29 +0800

    new commit

--- src/Linklist.java
+++ src/Linklist.java
@@ -108,6 +108,21 @@ public class Linklist {
         }
         return true;
     }
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null){
+            return false;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast!=slow){
+            if (slow.next == null || fast.next==null || fast.next.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
 }
 class ListNode {
     int val;
